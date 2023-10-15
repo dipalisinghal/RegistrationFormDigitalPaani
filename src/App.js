df90f80baeb749632d0d307f6188443cf279d912
dipali singhal <dsinghal7796@gmail.com>
2023-10-16 00:22:59 +0530

    DisplayData with local storage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
-
+import { useEffect, useState } from 'react';
 import RegistrationForm from './RegistrationForm/RegistrationForm'
-function App() {
+import DisplayData from './DisplayData/DisplayData';
+
+const getData = () => {
+    const LSdata = localStorage.getItem("digitalPaaniUser");
+    if (LSdata) {
+        return JSON.parse(LSdata);
+    }
+    else {
+        return []
+    }
+}
+
+ function App() {
 
     const [values, setValues] = useState({
         fullname: "",
@@ -55,18 +66,27 @@ function App() {
         },
 
     ];
-    const [allEntry, setAllEntry] = useState([]);
+    const [allEntry, setAllEntry] = useState(getData());
     const handleSubmit = (e) => {
         e.preventDefault();
-        if (values) {
-            const newEntry = { id: new Date().getTime().toString(), fullname: values.fullname, email: values.email, contact: values.contact, shortBio: values.shortBio }
-            localStorage.setItem("digitalPaaniUser", JSON.stringify([...allEntry, newEntry]))
+        const newEntry = { id: new Date().toString().substring(0,25), fullname: values.fullname, email: values.email, contact: values.contact, shortBio: values.shortBio }  
             setAllEntry([...allEntry, newEntry]);
-
-        }
+            setValues({
+                fullname: "",
+                email: "",
+                contact: "",
+                shortBio: "",
+            })
+          
 
     }
+    useEffect(() => {
+        localStorage.setItem("digitalPaaniUser", JSON.stringify(allEntry))
+        
+    }, [allEntry])
+   
 
+   
 
     const onChange = (e) => {
         setValues({ ...values, [e.target.name]: e.target.value });
@@ -91,42 +111,10 @@ function App() {
                 </form>
 
             </div>
-
-            {allEntry.length === 0 ?
-                (<h4 className="NoUser">No Registered User</h4>) :
-                (
-                    < div className="tabledata">
-                        <table>
-                            <tr className="ui">
-                                <th>Full Name</th>
-                                <th>Email</th>
-                                <th>Contact</th>
-                                <th>Bio</th>
-                                <th>DateTime</th>
-                            </tr>
-                            {
-                                allEntry.map((item) => {
-                                    return (
-                                        <tr classname="ui" style={{ color: "black" }} key={item.id}>
-                                            <td>{item.fullname}</td>
-                                            <td>{item.email}</td>
-                                            <td>{item.contact}</td>
-                                            <td>{item.shortBio}</td>
-                                            <td>{item.id}</td>
-                                            <hr />
-                                        </tr>
+           
 
 
-
-                                    )
-                                })
-                            }
-                        </table>
-                    </div>
-                )
-
-            }
-
+            <DisplayData allEntry={allEntry} />
         </>
     );
 }
---
+++ src/DisplayData/DisplayData.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { useState } from 'react'
+import './StyleData.css'
+
+export default function DisplayData({ allEntry }) {
+
+    return (
+        <>
+            <h1>hey</h1>
+            {allEntry.length === 0 ?
+                (<h4 className="NoUser">No Registered User</h4>) :
+                (
+                    < div className="tabledata">
+                        <table>
+                            <tr className="ui">
+                                <th>Full Name</th>
+                                <th>Email</th>
+                                <th>Contact</th>
+                                <th>Bio</th>
+                                <th>DateTime</th>
+                                
+                            </tr>
+                           
+                            {
+                                allEntry.map((item) => {
+                                    return (
+                                        <tr style={{ color: "black" }} key={item.id}>
+                                            <td className="data">{item.fullname}</td>
+                                            <td className="data">{item.email}</td>
+                                            <td className="data">{item.contact}</td>
+                                            <td className="data">{item.shortBio}</td>
+                                            <td className="data">{item.id}</td>
+                                            <hr />
+                                        </tr>
+
+
+
+                                    )
+                                })
+                            }
+                        </table>
+                    </div>
+                )
+
+            }
+        </>
+    )
+}
